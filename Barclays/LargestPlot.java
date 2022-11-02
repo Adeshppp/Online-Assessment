9d3962a484e59a87485d73b387e53095eaf68eb3
Adesh <adeshp948@gmail.com>
2022-11-02 17:44:47 -0400

    Update

--- Barclays/LargestPlot.java
+++ Barclays/LargestPlot.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class LargestPlot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of houses in a city: ");
+
         int size = sc.nextInt();
         int[][] houses = new int[size + 1][2];
         houses[0][0] = size;
